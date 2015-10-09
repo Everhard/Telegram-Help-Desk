424fea2350f6a6fbf5c0eb4a9d758fa9e6b71822
Andrew Dorokhov <andrew@dorohoff.net>
2015-10-09 03:30:17 +0300

    Add dismiss managers function (30 min).

--- control-roles/administrator.php
+++ control-roles/administrator.php
@@ -29,6 +29,37 @@ if ($user->hasActiveScenario($bot)) {
             
             break;
             
+        case "waiting-number-to-dismiss-manager":
+            
+            $DBH = DB::getInstance();
+            $stmt = $DBH->query("SELECT * FROM users WHERE is_manager = 1");
+
+            $users = $stmt->fetchAll();
+            
+            if (count($users) > 0) {
+                
+                $counter = 1;
+                
+                foreach ($users as $user_row) {
+                    
+                    if ($message->getSenderText() == $counter) {
+                        $DBH->exec("UPDATE users SET is_manager = 0 WHERE id = ".$user_row['id']);
+                        $DBH->exec("DELETE FROM bots WHERE manager_id = ".$user_row['user_telegram_id']);
+                        
+                        break;
+                    }
+                    
+                    $counter++;
+                } 
+            }
+            
+            $answer = "Менеджер был уволен!\n";
+            
+            $user->setScenarioDone($bot);
+            $user->sendMessage($answer, $bot);
+            
+            break;
+            
     }
 }
 
@@ -39,11 +70,46 @@ else {
 
         $answer = "Управление ботом Help Desk Center:\n\n";
         $answer .= "/who_is_admin - узнать, кто администратор.\n";
+        $answer .= "/dismiss_manager - уволить менеджера.\n";
     }
     
     if ($message->getSenderText() == "/who_is_admin") {
-
         $answer = "В данный момент Вы являетесь администратором.\n";
+    }
+    
+    if ($message->getSenderText() == "/dismiss_manager") {
+
+        $DBH = DB::getInstance();
+        $stmt = $DBH->query("SELECT * FROM users WHERE is_manager = 1");
+        
+        $users = $stmt->fetchAll();
+        
+        if (count($users) > 0) {
+            
+            $answer = "Укажите номер менеджера, которого нужно удалить:\n";
+            
+            $counter = 1;
+            
+            foreach($users as $user_row) {
+                $answer .= "$counter. $user_row[first_name] $user_row[last_name].\n";
+                $counter++;
+            }
+            
+            $buttons = array();
+            
+            for ($i = 1; $i <= count($users); $i++) {
+                $buttons[] = "$i";
+            }
+            
+            $user->sendMessage($answer, $bot, $buttons);
+            $user->setScenario("waiting-number-to-dismiss-manager", $bot);
+            
+            exit();            
+        }
+        
+        else {
+            $answer = "В данный момент не зарегистрировано ни одного менеджера!\n";
+        }
     }
 
     $user->sendMessage($answer, $bot);
--- gateway.php
+++ gateway.php
@@ -5,49 +5,52 @@ require_once('libs/telegram.php');
 require_once('libs/helpdesk.php');
 
 if ($input = file_get_contents('php://input')) {
-    
-    /*
-     * Message from user (it can be client, manager, admin).
-     */
-    $message = new Message($input);
-    
-    /*
-     * Current bot (it can be main admin bot or manager's bot):
-     */
-    $bot = new Bot();
-    
-    /*
-     * User that have sent message.
-     */
-    $user = new User(
-            $message->getSenderId(),
-            $message->getSenderFirstName(), 
-            $message->getSenderLastName()
-    );
+    try {
+        /*
+         * Message from user (it can be client, manager, admin).
+         */
+        $message = new Message($input);
 
-    if ($bot->isMain()) {
-        
-        if ($user->isAdmin()) {
-            require_once('control-roles/administrator.php');
+        /*
+         * Current bot (it can be main admin bot or manager's bot):
+         */
+        $bot = new Bot();
+
+        /*
+         * User that have sent message.
+         */
+        $user = new User(
+                $message->getSenderId(),
+                $message->getSenderFirstName(), 
+                $message->getSenderLastName()
+        );
+
+        if ($bot->isMain()) {
+
+            if ($user->isAdmin()) {
+                require_once('control-roles/administrator.php');
+            }
+
+            elseif ($user->isManager()) {
+                require_once('control-roles/manager.php');
+            }
+
+            elseif ($user->isClient()) {
+                require_once('control-roles/client.php');
+            }
+
         }
-        
-        elseif ($user->isManager()) {
-            require_once('control-roles/manager.php');
+
+        else {
+            if ($user->isManager($bot)) {
+                require_once('helpdesk-roles/manager.php');
+            }
+
+            if ($user->isClient($bot)) {
+                require_once('helpdesk-roles/client.php');
+            }
         }
-        
-        elseif ($user->isClient()) {
-            require_once('control-roles/client.php');
-        }
-        
-    }
-    
-    else {
-        if ($user->isManager($bot)) {
-            require_once('helpdesk-roles/manager.php');
-        }
-        
-        if ($user->isClient($bot)) {
-            require_once('helpdesk-roles/client.php');
-        }
+    } catch (Exception $ex) {
+        file_put_contents('debug.txt', $ex->getMessage()."\n", FILE_APPEND);
     }
 }
